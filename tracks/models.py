8a73c13794d85a82b04f59b8c77b66accd395725
Gopal <gopalsinghpanwar411@gmail.com>
2021-04-20 22:21:50 +0530

    added graphql jwt

--- tracks/models.py
+++ tracks/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+user = get_user_model()
 
 
 class Track(models.Model):
@@ -6,6 +9,7 @@ class Track(models.Model):
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     url = models.URLField()
+    posted_by = models.ForeignKey(user, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
